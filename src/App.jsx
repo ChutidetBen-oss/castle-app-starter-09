16b36ff609178d963b374f013d728192133cc375
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2025-12-04 12:08:26 +0700

    create:react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import { useState } from "react";
-import CastleRooms from "./compomnents/CastleRooms";
-import ViewToggleButton from "./compomnents/ViewTolggleBotton";
-import Castle from "./compomnents/01_Castle";
-import YouTubePlayer from "./compomnents/YoutubePlayer"
-// state variable vs standard JS variable (var, let, const)
-// In React we can use useState (a React Hook or built-in function/method) to create a state variable
-export default function App() {
-  const [view, setView] = useState("");
+import { createBrowserRouter, RouterContextProvider } from "react-router-dom";
 
-  return (
-    <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
-      <div className="flex gap-4 m-8">
-        <ViewToggleButton
-          onClick={()=>{setView("castle");
-          }}
-        >
-          Castle Rooms
-        </ViewToggleButton>
-        <ViewToggleButton
-          onClick={()=>{setView("video");
-          }}
-        >
-          Youtube Video
-        </ViewToggleButton>
-      </div>
-      {view==="castle" && <CastleRooms />}
-      {view==="video" && <YouTubePlayer />}
-    </div>
+const router = createBrowserRouter([])
+
+export default function App(){
+  return(
+
   );
 }
