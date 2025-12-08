46eccce843acadc9083ba739a20024a8bacc0968
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2025-12-08 16:09:18 +0700

    feat:add tick items

--- src/views/FetchAPI_EmptyArray.jsx
+++ src/views/FetchAPI_EmptyArray.jsx
@@ -3,6 +3,7 @@ export default function FetchAPI() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [ticketItems, setTicketItems] = useState(new Set())
     useEffect(() => {
         console.log("useEffect started"
 
@@ -26,6 +27,19 @@ export default function FetchAPI() {
         };
         fetchData();
     }, []);
+
+    const handleTick = (id) => {
+        setTicketItems((prev) => {
+            const newTickItems = new Set (prev)
+            if (newTickItems.has(id)) {
+                newTickItems.delete(id)
+            } else {
+                newTickItems.add(id)
+            }
+            return newTickItems
+        });
+    }
+
     if (loading) {
         return <div>Loading data...</div>;
     }
@@ -33,15 +47,19 @@ export default function FetchAPI() {
         <div>Error: {error.message}</div>
     }
     return (
-        <div>
-            <h1>Fetched Data from JSONPlaceholder</h1>
+        <div className="container mx-auto p-4">
+            <h1 className=" text-3xl font-bold mb-6 text-center">Fetched Data from JSONPlaceholder</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {data.map((item) => (
-                    <div key={item.id}>
+                    <div key={item.id} className={`p-6 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ${ticketItems.has(item.id) ? "bg-green-100 shadow-xl" : "bg-white"}`}>
                         <div className="flex justify-between items-start">
                             <h2 className="text-xl font-semibold mb-2 text-violet-700 pr-4">
                                 {item.title}
                             </h2>
+                            <input
+                            type="checkbox"
+                            onChange={()=> handleTick(item.id)}
+                            checked = {ticketItems.has(item.id)}/>
                         </div>
                         <p className="text-gray-700 mt-2">{item.body}</p>
                     </div>
